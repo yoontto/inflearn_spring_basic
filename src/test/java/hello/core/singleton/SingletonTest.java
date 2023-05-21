ecbea2c8d9b3e3566e1d4bf66641fd3ed23b6685
yoontto <jedney69@gmail.com>
2023-05-21 22:20:09 +0900

    5강 :: 다시 듣는중 - 싱글톤 컨테이너, 싱글톤 방식의 주의점

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
     
@@ -34,7 +36,7 @@ public class SingletonTest {
 
     public static void main(String[] args) {
         //싱글톤 객체 생성 안됨
-        //SingletonService singlrtonService1 = new SinglrtonService();
+        //SingletonService singletonService1 = new SingletonService();
     }
 
     @Test
@@ -49,4 +51,21 @@ public class SingletonTest {
         //1 == 2
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
     }
+    
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        //1은 2랑 다름
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }

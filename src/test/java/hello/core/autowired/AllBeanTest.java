97bb1962c1bbf9e8f52d8d14fe7020c0907f6d7d
yoontto <jedney69@gmail.com>
2023-05-29 19:37:05 +0900

    7강 :: 듣는 중 - 의존관계 자동 주입 - 조회한 빈이 모두 필요할때, Map / List 로 가져와 다형성 구현한 코드 짜기

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,60 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPolicy = discountService.discount(member, 20000, "rateDiscountPolicy");
+        assertThat(rateDiscountPolicy).isEqualTo(2000);
+
+    }
+
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            //생성자 만들면서 모든 policy들이 map에 들어가게 됨
+            //policyMap = {fixDiscountPolicy=hello.core.discount.FixDiscountPolicy@10a9d961, rateDiscountPolicy=hello.core.discount.RateDiscountPolicy@130e116b}
+            //policies = [hello.core.discount.HowDiscountPolicy@10a9d961, hello.core.discount.RateDiscountPolicy@130e116b]
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            //생성된 맵에서 서비스 찾아서 적용해줌
+            //다형성 구현
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}

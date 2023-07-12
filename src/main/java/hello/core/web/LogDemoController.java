6089d3571acde86599cbb08f2245e5820970b6fd
yoontto <jedney69@gmail.com>
2023-07-12 15:43:28 +0900

    9강 :: 끝 - 빈 스코프 - 스코프와 프록시

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
@@ -22,7 +22,12 @@ public class LogDemoController {
         //서비스 계층에는 웹 관련 정보를 가급적 넘기지 않는게 좋음
         //웹 관련 정보는 컨트롤러에서 처리하는 것이 유지보수 측면에서 좋음
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+
+        //myLogger :: class hello.core.common.MyLogger$$EnhancerBySpringCGLIB$$35e8c2a2
+        //CGLIB으로 클래스를 상속받은 가짜 프록시 객체를 만들어서 주입한다.
+        //가짜 프록시 객체는 요청이 오면 내부에서 진짜 빈을 요청하는 위임 로직이 있음
+        System.out.println("myLogger :: " + myLogger.getClass());
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
